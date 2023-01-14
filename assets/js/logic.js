2c33f6380e92fd89c4ea3ce58f1814c71ca16a81
sonia-deni <sonia_deni@hotmail.com>
2023-01-14 21:47:42 +0000

    Starter code and created a list of questions for the quiz, stored as an array of objects in the questions.js file

---
+++ assets/js/logic.js
@@ -0,0 +1,85 @@
+
+var startGame = document.querySelector("#start");
+startGame.addEventListener("click", begin);
+
+function begin(event){
+    console.log("game started");
+    playgame();
+}
+
+
+
+function decrementTimer(){
+
+}
+
+function playgame() {
+    
+
+}
+
+
+/*
+
+Psuedocode 
+
+Variables
+---
+
+score = 0; 
+
+
+1. We need a list of questions to be displayed on the page: these questions should be stored in the questions.js file. 
+
+    1.1 These questions should be stored within an array of objects. 
+    1.2 These objects should contain properties to store the buttons
+        - describe the object 
+
+        [
+
+            {
+                question: 'What data types can an array store ?',
+                choices: ['string', 'numbers', 'objects', 'all of them' ],
+                answer: 'all of them', 
+                correct: false
+            }, 
+
+            // multiple object questions 
+        ]
+
+2. logic file:  
+
+    functions 
+    --- 
+
+    1. game start function: add an event listener to the start button
+
+        1.1 - game will start with a question and timer will start
+        1.2 - get score from localStorage
+        1.3 - function should keep track of the questions - if all questions completed function should render to page their score and render html elements to save their initials and their final score. 
+
+    2  add a timer function
+    
+        2.1 - timer will decrement by seconds 
+        
+        2.2 - when an incorrect answer is submitted the timer will substract by an penalty amount e.g 1 min 
+    
+
+    3. get random question from array of objects 
+    4. check correct answer function 
+        
+        4.1 - when user gets the answer correct, mutate the object property correct to true
+    
+    
+    5. score function:  
+
+        5.1 - check if the object correct property is true, if true update the score variable 
+
+    6. 
+
+
+3. Highscores file
+
+    1. Do something with local storage to get/set the highscores
+    2. Check if higher than existing highscore, if yes, add to list, if no then don't
+*/
---
+++ assets/js/questions.js
@@ -0,0 +1,83 @@
+
+var questions = [
+    {
+        Question: 'What is JavaScript?',
+        ChoiceA: 'JavaScript is a scripting language used to make the website interactive',
+        ChoiceB: 'JavaScript is an assembly language used to make the website interactive',
+        ChoiceC: 'JavaScript is a compiled language used to make the website interactive',
+        ChoiceD: 'None of the above',
+        CorrectAnswer: 'JavaScript is a scripting language used to make the website interactive'
+    },
+    {
+        Question: 'Arrays in JavaScript are defined by which of the following statements?',
+        ChoiceA: 'It is an ordered list of values',
+        ChoiceB: 'It is an ordered list of objects',
+        ChoiceC: 'It is an ordered list of strings',
+        ChoiceD: 'It is an ordered list of any of the above',
+        CorrectAnswer: 'It is an ordered list of any of the above'
+    },
+    {
+        Question: 'Which of the following is not a type of loop in JavaScript?',
+        ChoiceA: 'While loop',
+        ChoiceB: 'For loop',
+        ChoiceC: 'If loop',
+        ChoiceD: 'Do While loop',
+        CorrectAnswer: 'If loop'
+    },
+    {
+        Question: 'What is a function?',
+        ChoiceA: 'A set of statements that performs a task or calculates a value',
+        ChoiceB: 'An instruction to print to the console',
+        ChoiceC: 'A type of variable',
+        ChoiceD: 'A collection of data',
+        CorrectAnswer: 'A set of statements that performs a task or calculates a value'
+    },
+    {
+        Question: 'Which of the following is a type of number?',
+        ChoiceA: 'Boolean',
+        ChoiceB: 'Sink',
+        ChoiceC: 'Float',
+        ChoiceD: 'String',
+        CorrectAnswer: 'Float'
+    },
+    {
+        Question: 'How could you take user input as a string?',
+        ChoiceA: 'Using an alert',
+        ChoiceB: 'Using a prompt',
+        ChoiceC: 'Using a confirm',
+        ChoiceD: 'Using a popup',
+        CorrectAnswer: 'Using a prompt'
+    },
+    {
+        Question: 'Which of these is not a logical operator?',
+        ChoiceA: 'True',
+        ChoiceB: 'Not',
+        ChoiceC: 'Or',
+        ChoiceD: 'And',
+        CorrectAnswer: 'True'
+    },
+    {
+        Question: 'A variable can be declared using which of the following?',
+        ChoiceA: 'Put',
+        ChoiceB: 'Set',
+        ChoiceC: 'This',
+        ChoiceD: 'Let',
+        CorrectAnswer: 'Let'
+    },
+    {
+        Question: 'A block of code is contained between',
+        ChoiceA: 'Brackets',
+        ChoiceB: 'Curly braces',
+        ChoiceC: 'Quotation marks',
+        ChoiceD: 'It does not need anything around it',
+        CorrectAnswer: 'Curly braces'
+    },
+    {
+        Question: 'Which of these is not a String Method?',
+        ChoiceA: 'Split',
+        ChoiceB: 'Trim',
+        ChoiceC: 'CharAt',
+        ChoiceD: 'IntAt',
+        CorrectAnswer: 'IntAt'
+    },
+];
